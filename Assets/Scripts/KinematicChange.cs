8f422cb4ad4b41d86f2b8a5108d89e73f36db186
Antone Abuyaghi <aabuyagh@gmail.com>
2017-11-10 13:28:01 -0800

    Obstacles/Camera Script/Kinematic Change Script

---
+++ Assets/Scripts/KinematicChange.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicChange : MonoBehaviour {
+
+    // Use this for initialization
+    private Rigidbody rb;
+    private Rigidbody ob;
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody>())
+        {
+            ob = col.gameObject.GetComponent<Rigidbody>();
+        }
+        if (ob.isKinematic == false)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}
