807ed869da09132b9abc646c19a8d5f2c2321077
Antone Abuyaghi <aabuyagh@gmail.com>
2017-11-19 12:23:59 -0800

    Active Ragdoll Initial setup

---
+++ Assets/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb.AddForce(movement * speed);
+    }
+}
